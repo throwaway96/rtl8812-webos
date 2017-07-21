9908ce2fc26fbdc75c7f03eeda0623a2761a0556
Ouden.Lin <ouden.lin@realtek.com>
2017-07-21 11:18:44 +0800

    Enable Layer 2 active roaming
    :Release Notes:
    Enable Layer 2 active roaming
    
    :Detailed Notes:
    We enable Layer 2 active roaming,
    that when the signal level is very lower,
    the device connect other strong signal AP.
    default threshold is -70 dBm
    
    :Testing Performed:
    
    :Issues Addressed:
    Request form Pyeongtaek.
    The condition is that there are 2 or 3 AP that have same SSID.
    Case #1 : After connection with one AP, when the signal level is very lower, the device need to connect other AP.
    
    Change-Id: Idad8eb54c095464cee03d6caed63f78e12f9c9c2

--- include/autoconf.h
+++ include/autoconf.h
@@ -205,6 +205,7 @@
 
 #define CONFIG_LAYER2_ROAMING
 #define CONFIG_LAYER2_ROAMING_RESUME
+#define CONFIG_LAYER2_ROAMING_ACTIVE
 /*#define CONFIG_ADAPTOR_INFO_CACHING_FILE */ /* now just applied on 8192cu only, should make it general... */
 /*#define CONFIG_RESUME_IN_WORKQUEUE */
 /*#define CONFIG_SET_SCAN_DENY_TIMER */
