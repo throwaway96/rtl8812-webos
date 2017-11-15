f4acfe029ccb40db4a5dab24adc8a4048452b0fc
Ouden.Lin <ouden.lin@realtek.com>
2017-11-15 13:24:05 +0800

    Fix connection fail issue with ASUS ZenPC
    :Release Notes:
    Fix connection fail issue with ASUS ZenPC
    
    :Detailed Notes:
    update FW to resolve MCC issue for connection fail issue with ASUS ZenPC
    When ASUS ZenPC become GO, it will carry NoA count = 1 in Beacon.
    FW add this condition.
    
    :Testing Performed:
    Verify OK on M3
    
    :Issues Addressed:
    (WOSQRTKW-9)
    [QEVENTSEVT-20974][Compatibility.Device_WiDi.호환성] [Always] [Minor] 연결안됨
    
    Change-Id: I9290ab8e8f4e85b6cb0a9c5fbe99cadb1aa8f4c9

--- hal/rtl8822b/hal8822b_fw.h
+++ hal/rtl8822b/hal8822b_fw.h
@@ -25,15 +25,15 @@
 
 #ifdef LOAD_FW_HEADER_FROM_DRIVER
 #if (defined(CONFIG_AP_WOWLAN) || (DM_ODM_SUPPORT_TYPE & (ODM_AP)))
-extern u8 array_mp_8822b_fw_ap[82736];
+extern u8 array_mp_8822b_fw_ap[82744];
 extern u32 array_length_mp_8822b_fw_ap;
 #endif
 
 #if (DM_ODM_SUPPORT_TYPE & (ODM_WIN)) || (DM_ODM_SUPPORT_TYPE & (ODM_CE))
-extern u8 array_mp_8822b_fw_nic[133992];
+extern u8 array_mp_8822b_fw_nic[134184];
 extern u32 array_length_mp_8822b_fw_nic;
 #ifdef CONFIG_WOWLAN
-extern u8 array_mp_8822b_fw_wowlan[76944];
+extern u8 array_mp_8822b_fw_wowlan[76992];
 extern u32 array_length_mp_8822b_fw_wowlan;
 #endif /*CONFIG_WOWLAN*/
 #endif
