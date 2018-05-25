1247537fbc96d03ab7d32312dce0e8c4d4b77e6c
Ouden Lin <ouden.lin@realtek.com>
2018-05-25 10:22:13 +0800

    Enable low power consumption funtion.
    :Release Notes:
    Enable low power consumption funtion.
    
    :Detailed Notes:
    Enable low power consumption funtion.
    
    :Testing Performed:
    Compiler Pass and test on M3.
    
    :Issues Addressed:
    (RTKWBTV-333)
    	[8812BU][19Y_Initial] Wi-Fi Driver verification process
    
    Change-Id: If92cf55f20fdd1289aa5e7a1b82b2823a12cd72c

--- include/autoconf.h
+++ include/autoconf.h
@@ -92,12 +92,12 @@
 
 	#define CONFIG_LPS	1
 	#if defined(CONFIG_LPS)
-		/* #define CONFIG_LPS_LCLK	1 */
+		#define CONFIG_LPS_LCLK	1
 	#endif
 
 	#ifdef CONFIG_LPS_LCLK
 		#ifdef CONFIG_POWER_SAVING
-			#define CONFIG_XMIT_THREAD_MODE
+			/* #define CONFIG_XMIT_THREAD_MODE */
 		#endif /* CONFIG_POWER_SAVING */
 		#ifndef CONFIG_SUPPORT_USB_INT
 			#define LPS_RPWM_WAIT_MS 300
