821ed9b88d660a26e67598ba7856779321a59c0e
Ouden Lin <ouden.lin@realtek.com>
2021-04-26 15:20:07 +0800

    Kernel 5.4 supported, Part 3
    :Release Notes:
    Kernel 5.4 supported
    
    :Detailed Notes:
    Request
    LGE will upgrade kernel 4.4 to 5.4
    This patch will be worked on both kernel 4.4 and 5.4
    
    This patch :
    do not call rtw_cfgvendor_attach and rtw_cfgvendor_detach for 5.4
    
    :Testing Performed:
    check on k6lp and o20n
    
    :Issues Addressed:
    RTKWBTV-1325
    [RTK][webOS22] Share Kernel/wpa-supplicant/toolchain
    
    Change-Id: I5f7c423730eb536a435d2c545013a90f7b7a8b98
    Signed-off-by: Ouden Lin <ouden.lin@realtek.com>

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -10018,7 +10018,9 @@ int rtw_wiphy_register(struct wiphy *wiphy)
 {
 	RTW_INFO(FUNC_WIPHY_FMT"\n", FUNC_WIPHY_ARG(wiphy));
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)) || defined(RTW_VENDOR_EXT_SUPPORT)
+#if ( ((LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)) &&  \
+	LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)) \
+	|| defined(RTW_VENDOR_EXT_SUPPORT) )
 	rtw_cfgvendor_attach(wiphy);
 #endif
 
@@ -10029,7 +10031,9 @@ void rtw_wiphy_unregister(struct wiphy *wiphy)
 {
 	RTW_INFO(FUNC_WIPHY_FMT"\n", FUNC_WIPHY_ARG(wiphy));
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)) || defined(RTW_VENDOR_EXT_SUPPORT)
+#if ( ((LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)) &&  \
+	LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)) \
+	|| defined(RTW_VENDOR_EXT_SUPPORT) )
 	rtw_cfgvendor_detach(wiphy);
 #endif
 
